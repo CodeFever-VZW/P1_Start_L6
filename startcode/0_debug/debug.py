c7555bc00883c40b661b6eb978a0ef997c7e5b10
Tessa <48088982+Tesax123@users.noreply.github.com>
2025-02-11 20:04:21 +0100

    Create debug.py

---
+++ startcode/0_debug/debug.py
@@ -0,0 +1,16 @@
+aantal_punten = input("Hoeveel punten moet de ster hebben? ")
+
+t = turtle.Turltle()
+t.speed(4)
+
+for i in range(aantal_punten)
+    if i % 2 == 0:
+        t.color("red")
+    else:
+    t.color("blue")
+
+    t.forward(200)
+    t.right(180 - 180 / aantalpunten)
+
+t.hideturtle()
+turtle.done()
